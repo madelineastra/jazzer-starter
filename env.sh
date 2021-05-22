8f577a38ed881c3a79dfcbcf222f5c05db3b8bfb
Madeline Smith <madeline.astra@gmail.com>
2021-05-22 15:24:39 -0700

    Removing old JQF files

--- env.sh
+++
@@ -1,6 +0,0 @@
-export PROJECT_DIR=/home/user/Projects/public
-export AFL_HOME=$PROJECT_DIR/AFLplusplus
-export AFL_DIR=$AFL_HOME
-export JQF_HOME=$PROJECT_DIR/jqf
-export PATH=$PATH:$AFL_HOME:$JQF_HOME/bin
-bash
